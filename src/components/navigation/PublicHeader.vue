0487c4a32a82ba096ef3f3e5c98fb62feb26e4da
Gracias <gracias.claude@outlook>
2021-03-14 08:54:09 -0600

    manually add tailwind css to codebase and updated UI

--- src/components/navigation/PublicHeader.vue
+++ src/components/navigation/PublicHeader.vue
@@ -1,6 +1,9 @@
 <template lang="">
     <div>
         <h1>Nav</h1>
+        <div class="flex justify-center">
+    <p class="btn rounded-md p-2 shadow-md text-color cursor-pointer">Tailwind</p>
+  </div>
     </div>
 </template>
 <script>
