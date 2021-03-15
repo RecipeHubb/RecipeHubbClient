574f2ac51adf8224446e8645e5e905afed75aa54
Gracias <gracias.claude@outlook>
2021-03-14 23:04:49 -0600

    added a new homeepagee and mobile functionality

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -1,7 +1,7 @@
 <template lang="">
-    <div>
-        <h1>Footer</h1>
-    </div>
+   <div class='bg-purple-300 w-full text-center border-t border-grey p-4 pin-b absolute inset-x-0 bottom-0 h-16 '>
+            Footer
+        </div>
 </template>
 <script>
 export default {
--- src/components/navigation/PublicHeader.vue
+++ src/components/navigation/PublicHeader.vue
@@ -1,16 +1,84 @@
 <template lang="">
-    <div>
-        <h1>Nav</h1>
-        <div class="flex justify-center">
-    <p class="btn rounded-md p-2 shadow-md text-color cursor-pointer">Tailwind</p>
-  </div>
+  <div>
+    <div class="bg-hue h-full font-serif flex-1">
+      <!-- NAVBAR -->
+      <div
+        class="lg:px-6 py-2 my-2 px-4 border-b-2 border-gray-600 flex justify-between"
+      >
+        <!-- <i class="fa fa-fire fa-lg"></i> -->
+        <div>
+          <img
+            width="50px"
+            height="50px"
+            alt="Vue logo"
+            src="https://recipehubb.s3.us-east-2.amazonaws.com/assets/recipehubb_main.png"
+          />
+
+          <!-- show this section if theey click on the hamburger-->
+          <div class="pt-3" v-show="isOpen">
+            <nav class="cursor-pointer">
+              <ul>
+                <li class="border-transparent border-b-2 hover:border-purple-600">
+                  Home
+                </li>
+                <li class="border-transparent border-b-2 hover:border-purple-600">
+                  About
+                </li>
+                <li class="border-transparent border-b-2 hover:border-purple-600">
+                  Sign in
+                </li>
+                <li class="border-transparent border-b-2 hover:border-purple-600">
+                  Sign up
+                </li>
+              </ul>
+            </nav>
+          </div>
+          
+        </div>
+        <div class="inline lg:hidden mt-1 ml-2">
+          <label v-on:click="handleMenu"> <feather-activity></feather-activity> </label>
+        </div>
+        <input type="checkbox" id="menu-toggle" class="mt-4 hidden" />
+
+        <div class="lg:inline lg:float-right hidden">
+          <button
+            class="px-2 border-transparent border-b-2 hover:border-purple-600 focus:outline-none outline-none"
+          >
+            Home
+          </button>
+          <button
+            class="px-2 border-transparent border-b-2 hover:border-purple-600 focus:outline-none outline-none"
+          >
+            About
+          </button>
+          <button
+            class="px-2 border-transparent border-b-2 hover:border-purple-600 focus:outline-none outline-none"
+          >
+            Sign in
+          </button>
+          <button
+            class="px-2 border-transparent border-b-2 hover:border-purple-600 focus:outline-none outline-none"
+          >
+            Sign up
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'PublicHeader'
+    name: 'PublicHeader',
+    data()  {
+    return {
+      isOpen: false
+        }
+    },
+    methods: {
+    handleMenu () {
+      this.isOpen = !this.isOpen
+    }
+        }
 }
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
