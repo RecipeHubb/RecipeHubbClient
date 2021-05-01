9b561c7bc8aa48138f50286ab0758aefc6994221
cpaulson09 <cpaulson09@hotmail.com>
2021-04-30 22:07:11 -0600

    added error page to router | style changes

--- src/components/public/404.vue
+++ src/components/public/404.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <div class="h-full lg:p-16 py-16">
-      <div class="shadow-lg rounded-lg bg-indigo-50">
+      <div class="shadow-lg rounded-lg bg-purple-300">
         <div class="font-mono flex justify-center">
           <div class="text-center lg:m-16 m-8 lg:w-96">
               <h1 class="text-2xl py-2">Error: Page not Found</h1>                
