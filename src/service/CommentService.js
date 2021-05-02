7cadd11853a8338b27f7bab24ed3aa0d89983156
Gracias <gracias.claude@outlook>
2021-05-01 18:20:08 -0600

    add vue-cli and remove unneeded code for commentservice

--- src/service/CommentService.js
+++ src/service/CommentService.js
@@ -6,33 +6,4 @@ if (process.env.NODE_ENV === "development") {
 }
 module.exports = {
 
-    register: (data) => {
-        console.log(data)
-        try {
-            axios.post(url, {
-                username: data.username,
-                email: data.email,
-                password: data.password
-            })
-        }
-        catch(err){
-            console.log(err)
-        }
-        
-    },
-
-    login: (data) => {
-        console.log(data)
-        try {
-            axios.post(url, {
-                username: data.username,
-                email: data.email,
-                password: data.password
-            })
-        }
-        catch(err){
-            console.log(err)
-        }
-        
-    },
 }
