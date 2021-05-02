39ffdeb08d3748b6a9986ac4d47eecfd18acc8aa
Gracias <gracias.claude@outlook>
2021-05-02 11:14:25 -0600

    comment out commentsseervice

--- src/service/CommentService.js
+++ src/service/CommentService.js
@@ -1,9 +1,9 @@
-// Login/Signup and Auth service page
-const axios = require('axios')
-let URL = process.env.API_URL || 'https://recipehubbapi.herokuapp.com/'
-if (process.env.NODE_ENV === "development") {
-  URL = "http://localhost:8000/";
-}
-module.exports = {
+// // Login/Signup and Auth service page
+// const axios = require('axios')
+// // let URL = process.env.API_URL || 'https://recipehubbapi.herokuapp.com/'
+// if (process.env.NODE_ENV === "development") {
+//   URL = "http://localhost:8000/";
+// }
+// module.exports = {
 
-}
+// }
