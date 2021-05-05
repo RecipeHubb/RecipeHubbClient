251bfe65fcf2e9d1dcf341a39a748c89c41956a8
Gracias <gracias.claude@outlook>
2021-05-05 01:48:46 -0600

    extra merge

--- src/components/platform/publicRecipes/Search.vue
+++ src/components/platform/publicRecipes/Search.vue
@@ -276,10 +276,6 @@ export default {
         return;
       }
       if (this.selectedQuickFilter.value == "username" || this.userName) {
-<<<<<<< HEAD
-=======
-        //username
->>>>>>> staging
         // not null
         if (this.userInput || this.userName) {
           this.recipeData.userName = this.userInput || this.userName;
